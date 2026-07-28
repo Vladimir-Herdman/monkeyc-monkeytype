18b1ab4b64babb575172df421e0d9be756bc1053
Vladimir Herdman <vovaherdman@gmail.com>
2026-07-28 14:30:16 -0400

    Settings screen setup

---
+++ src/screens/settings.h
@@ -0,0 +1,9 @@
+#ifndef MCMT_SCREENS_SETTINGS_H
+#define MCMT_SCREENS_SETTINGS_H
+
+//TODO: colorblind mode option, that changes the success/wrong colors to
+//more colorblind-friendly options, like purple and yellow (I think
+//those are good choices).
+
+
+#endif //MCMT_SCREENS_SETTINGS_H
